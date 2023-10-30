fe02f570780298d34d295291834e3bc86f48e7cb
Albert Cordina <albert.cordina@dci-student.org>
2023-10-30 13:37:22 +0100

    update

--- Junitdemo/src/main/java/com/example/Junitdemo/Calculator.java
+++ Junitdemo/src/main/java/com/example/Junitdemo/Calculator.java
@@ -2,9 +2,9 @@ package com.example.Junitdemo;
 
 public class Calculator {
 	
-	int add(int a, int b) {return a + b;} // for addition.
-	int subtract(int a, int b) {return a - b;} // for subtraction.
-	int multiply(int a, int b) {return a * b;} // for multiplication.
-	int divide(int a, int b) {return a / b;} // for integer division.
+	public static int add(int a, int b) {return a + b;} // for addition.
+	public static int subtract(int a, int b) {return a - b;} // for subtraction.
+	public static int multiply(int a, int b) {return a * b;} // for multiplication.
+	public static int divide(int a, int b) { if (b == 0) {return 0;}return a / b;} // for integer division.
 
 }
--- Junitdemo/src/test/java/com/example/Junitdemo/CaluclatorTest.java
+++ Junitdemo/src/test/java/com/example/Junitdemo/CaluclatorTest.java
@@ -1,28 +1,52 @@
 package com.example.Junitdemo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class CaluclatorTest {
 
 	@Test
-	void addTest() {
+	public void addTest1() {
 		Calculator calculate = new Calculator();
 		assertEquals (4, calculate.add (2, 2));	
 	}
 	@Test
-	void subtractTest() {
+	public void addTest2() {
+		Calculator calculate = new Calculator();
+		assertEquals (6, calculate.add (3, 3));	
+	}
+	
+	@Test
+	public void subtractTest1() {
 		Calculator calculate = new Calculator();
 		assertEquals (0, calculate.subtract (2, 2));	
 	}
 	@Test
-	void multiplyTest() {
+	public void subtractTest2() {
+		Calculator calculate = new Calculator();
+		assertEquals (3, calculate.subtract (5, 2));	
+	}
+	
+	@Test
+	public void multiplyTest1() {
 		Calculator calculate = new Calculator();
 		assertEquals (4, calculate.multiply (2, 2));	
 	}
 	@Test
-	void divisionTest() {
+	public void multiplyTest2() {
+		Calculator calculate = new Calculator();
+		assertEquals (12, calculate.multiply (3, 4));		
+	}	
+	
+	@Test
+	public void divisionTest1() {
 		Calculator calculate = new Calculator();
 		assertEquals (1, calculate.divide (2, 2));	
+	}
+	@Test
+	public void divisionTest2() {
+		Calculator calculate = new Calculator();
+		assertEquals (2, calculate.divide (4, 2));	
 	}
 }
