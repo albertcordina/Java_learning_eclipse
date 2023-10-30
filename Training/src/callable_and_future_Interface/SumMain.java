bfec1ca7d772b66aa72bbbc6eec791549e5dc5cd
Albert Cordina <albert.cordina@dci-student.org>
2023-10-30 17:21:42 +0100

    update

--- Training/src/callable_and_future_Interface/SumMain.java
+++ Training/src/callable_and_future_Interface/SumMain.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-
+//----------------------------------------------------------------------
 class SumCalculator implements Callable <Integer> {
 	
 	int start, end;
@@ -22,7 +22,7 @@ class SumCalculator implements Callable <Integer> {
 		return sum;
 	}
 }
-
+//------------------------------------------------------------------------
 public class SumMain {
 	
 	public static void main(String[] args) throws InterruptedException {
