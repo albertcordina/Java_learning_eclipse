f1d8659d1089566ad92be6f8872e56e61936c6b1
Albert Cordina <albert.cordina@dci-student.org>
2023-11-02 13:02:03 +0100

    update

--- Junitdemo/src/test/java/com/example/Junitdemo/PaymentProcessorStubTest.java
+++ Junitdemo/src/test/java/com/example/Junitdemo/PaymentProcessorStubTest.java
@@ -1,13 +1,14 @@
 package com.example.Junitdemo;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 public class PaymentProcessorStubTest {
+	
     @Test
     public void testMakePaymentWithStub() {
         PaymentProcessor paymentProcessor = new PaymentProcessor(new PaymentGateway() {
+        	
             @Override
             public boolean processPayment(double amount) {
                 return true; // Simulate a successful payment
