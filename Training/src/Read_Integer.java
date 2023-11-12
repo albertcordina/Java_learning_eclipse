72c4403f981c1f2d4464c17d73ea54d5786cb560
Albert Cordina <albert.cordina@dci-student.org>
2023-11-12 19:43:19 +0100

    update

---
+++ Training/src/Read_Integer.java
@@ -0,0 +1,12 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+// "//home//dci-student//eclipse-workspace//SmartCitySimulation//resources//traffic_data.csv"));
+public class Read_Integer {
+	
+  public static void main(String args[]) throws IOException {
+
+  }
+}
--- Training/src/array/Arraylist_of_objects.java
+++ Training/src/array/Arraylist_of_objects.java
@@ -13,9 +13,17 @@ public class Arraylist_of_objects {
 		cars.add(new Car ("BMW", 8635, "black"));
 		
 		cars.set(2, new Car ("Kia", 2365, "white")); // we can change the value(s) of an element, e.g. '2'
+		//System.out.println(cars.remove(2)); // we can also remove the object '2' from the list
 		
-		for (Car auto : cars) { System.out.println(auto); } // to print out the whole list
-		//System.out.println(cars.remove(2)); // we can remove an element from the list
+		Car element = cars.get(2); // we can get only the first element of the object '2'
+		System.out.println(element);
+		
+		for (Car auto : cars) { System.out.println(auto); } // to print out only the first elements of the whole list of objects
+		
+//---------------------------- getting a certain element from a certain object ----------------------------------------		
+		System.out.println(cars.get(2).getSerialNumber()); // to print out the element int 'serialNumber' of the object '2'
+		System.out.println(cars.get(0).getColor()); // to print out the element String 'color' of the object '0'
+		
 	}
 }
 
@@ -32,4 +40,28 @@ public class Arraylist_of_objects {
 	}
 	
 	public String toString() { return brandName; }
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public int getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(int serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 }
