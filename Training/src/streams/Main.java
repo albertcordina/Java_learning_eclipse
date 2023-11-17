b53bb829bf72f275a0c2f40cea8ee27e1185fd28
Albert Cordina <albert.cordina@dci-student.org>
2023-11-17 17:55:03 +0100

    update

--- Training/src/streams/Main.java
+++ Training/src/streams/Main.java
@@ -6,23 +6,5 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		List <String> n = new ArrayList <> ();
-		
-		n.add("One");
-		n.add("Two");
-		n.add("Three");
-		n.add("Four");
-
-		//n.stream().forEach(System.out :: println);
-		//n.forEach(System.out :: println); // it can be also without 'stream'	 
-		//n.parallelStream().forEach(System.out :: println); // with 'parallelStream' it is faster
-		
-		IntStream ordered = IntStream.rangeClosed(1, 10);
-		ordered.forEach(System.out :: println);
-		
-		IntStream unOrdered = IntStream.rangeClosed(1, 10);
-		unOrdered.parallel().forEach(System.out :: println);
-	}
+	
 }
