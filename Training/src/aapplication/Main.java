2a287f4734965968163af3795aa43177ad443e08
Albert Cordina <albert.cordina@dci-student.org>
2023-11-29 16:11:51 +0100

    fg

--- Training/src/aapplication/Main.java
+++ Training/src/aapplication/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Dear applicant, tank you contacting us, we are happy to help you with your application!");
+		System.out.println("Dear applicant, tank you contacting us,\nwe are happy to help you with your application!");
 		mainMenu();
 	}
 
@@ -34,7 +34,7 @@ public class Main {
 					collectionInfo.collection();
 					break;
 				case 2:
-					registered.account();
+					registered.accessAccount();
 					break;
 				case 0:
 					System.out.println("\nThank you for visiting us!");
@@ -62,7 +62,7 @@ class Methods {
 		System.out.print("Please, enter your name: ");
 		String input = scanner.nextLine();
 
-		while (!isAllLetters(input.trim())) {
+		    while (!isAllLetters(input.trim())) {
 			System.out.print("Invalid input. Please enter only letters for your name: ");
 			input = scanner.nextLine();
 
@@ -79,7 +79,7 @@ class Methods {
 		int userInput = 0;
 		do {
 			try {
-				System.out.print("Please, enter a positive valid number: ");
+				System.out.print("Please, enter: ");
 				userInput = scanner.nextInt();
 
 				if (userInput <= 0) {
@@ -108,7 +108,8 @@ class Methods {
 		return input;
 	}
 
-	// Takes only the existed phone numbers which contain from 7 to 15 digits (including a country code)
+	// Takes only the existed phone numbers which contain from 7 to 15 digits
+	// (including a country code)
 	public static int getValidPhoneNumber(Scanner scanner) {
 
 		while (true) {
@@ -134,7 +135,8 @@ class Methods {
 	 * at least 12 characters long and has the combination of at least one uppercase
 	 * letter, lowercase letter, number, and a symbol.
 	 */
-	public static String getValidPassword(Scanner scanner) { // Repeatedly prompts the user until a valid password is entered.
+	public static String getValidPassword(Scanner scanner) { // Repeatedly prompts the user until a valid password is
+																// entered.
 
 		while (true) {
 			System.out.println(
@@ -185,24 +187,30 @@ class Methods {
 	}
 
 	public void createAccount() {
+
 		Scanner scanner = new Scanner(System.in);
 
 		// Collect the contact phone number of the Applicant
-		System.out.println("Dear " + Info.name
+		System.out.println("\nDear " + Info.name
 				+ ",\nWe are happy to inform you that your application will be reviewed by our specialized team.\n"
 				+ "In order to create your personal account, we need to have your personal contact phone number.");
 		Info.phoneNumber = getValidPhoneNumber(scanner);
 
-		if (Info.list.containsKey(Info.phoneNumber)) { // If the phone number is in the list, send the Applicant to the Class Registered
+		if (Info.profiles.containsKey(Info.phoneNumber)) { // If the phone number is in the list, send the Applicant to
+															// the
+															// Class Registered
 			System.out.println("It looks that you are already registered.");
-			Registered.account();
+			Registered.accessAccount();
 		}
 
 		// Create the password
-		System.out.println("Thank you!\nPlease, create your password.");
+		System.out.println("\nThank you!\nPlease, create your password.");
 		Info.password = getValidPassword(scanner);
 
-		Info.list.put(Info.phoneNumber, new Info(Info.name, Info.age, Info.password, Info.status)); // placing the info into the 'list'
+		Info.profiles.put(Info.phoneNumber, new Info(Info.name, Info.age, Info.password, Info.status)); // placing the
+																										// collected
+																										// info
+		// into the 'list'
 
 		System.out.println(Info.name
 				+ ", thank you for entering your personal details.\nYour account with us has been successfully created."
@@ -213,7 +221,7 @@ class Methods {
 
 class Info extends Methods {
 
-	static String name;
+	protected static String name;
 	static int age;
 	static String password;
 	static String status;
@@ -223,7 +231,7 @@ class Info extends Methods {
 	static String yesOrNot;
 
 	static Scanner scanner = new Scanner(System.in);
-	static Map<Integer, Info> list = new HashMap<>();
+	static Map<Integer, Info> profiles = new HashMap<>();
 
 	public Info(String name, int age, String password, String status) { // constructor
 		this.name = name;
@@ -232,31 +240,49 @@ class Info extends Methods {
 		this.status = status;
 	}
 
-	public Info() { }
+	public Info() {
+	}
 
 	public String toString() {
 		return "\nYour name: " + name + "\nYour age: " + age + "\nThe password to your account: " + password
 				+ "\nThe status of the application: " + status;
 	} // toString method
-	
-	 public static String getName() { return name; }
-	 
-	 public static void setName(String name) { Info.name = name; }
-	 
-	 public static int getAge() { return age; }
-	 
-	 public static void setAge(int age) { Info.age = age; }
-	  
-	 public static String getPassword() { return password; }
-	  
-	 public static void setPassword(String password) { Info.password = password; }
-	  
-	 public static String getStatus() { return status; }
-	  
-	 public static void setStatus(String status) { Info.status = status; }
-	 
-	 public static int getPhoneNumber() { return phoneNumber; }
-	 
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		Info.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		Info.age = age;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		Info.password = password;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		Info.status = status;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
 
 	public static void collection() {
 
@@ -271,12 +297,12 @@ class Info extends Methods {
 		age = getPositiveInt(scanner);
 
 		// Ask the Applicant about a current support
-		System.out.print("\nAre you getting a current support from the government?");
+		System.out.println("\nAre you getting a current support from the government?");
 		yesOrNot = yesOrNot(scanner);
 
 		if (yesOrNot.equals("yes")) {
 			System.out
-					.print("\nThank you!\nWhat is the monthly amount you are currently getting from the government?\n");
+					.println("\nThank you!\nWhat is the monthly amount you are currently getting from the government?");
 			monthlySupport = getPositiveInt(scanner);
 
 			if (monthlySupport >= 2000) {
@@ -287,32 +313,26 @@ class Info extends Methods {
 		}
 
 		// Ask the Applicant who are over 67 years old about the pension
+		Retired retired = new Retired();
 		if (age >= 67) {
-			System.out.print(
+			System.out.println(
 					"\nAccording to your age, you are eligible for getting a pension.\nHave you applied for your pension?");
 			yesOrNot = yesOrNot(scanner);
-		}
 
-		if (yesOrNot.equals("yes")) {
-			System.out.print("\nThank you!\nWhat is the monthly amount of your pension?\n");
-			monthlyPension = getPositiveInt(scanner);
+			if (yesOrNot.equals("yes")) {
+				retired.eligibility();
+			} else {
+				System.out.println("\n\tDear " + name
+						+ ",\nAccording to your age and the regulation of the social service support you are "
+						+ "eligible for the retirement pension. \nTherefore, we advise you fist to contact your local social servise for "
+						+ "clarification of this subject and then contact us again.\nThank you for visiting us!");
 
-			if (monthlyPension + monthlySupport >= 2000) { // we check the Applicant's income altogether
-				System.out.println("\n\tDear " + name + ",\nThe amount of your income is considered as sufficient and "
-						+ "it fully covers the amount stipulated within this support program.\nThank you for visiting us!");
 				System.exit(0);
+
 			}
-		}
-		if (yesOrNot.equals("not")) {
-			System.out.println("\n\tDear " + name
-					+ ",\nAccording to the regulation of the social service support you are "
-					+ "eligible for the retirement pension. \nTherefore, we advise you fist to contact your local social servise for "
-					+ "clarification of this subject and then contact us again.\nThank you for visiting us!");
-			System.exit(0);
 		}
 
 		// The Occupational status of the Applicant
-		Retired retired = new Retired();
 		Student student = new Student();
 		Employed employed = new Employed();
 		Unemployed unemployed = new Unemployed();
@@ -339,7 +359,7 @@ class Info extends Methods {
 					unemployed.eligibility();
 					break;
 				case 4:
-					retired.eligiblility();
+					retired.eligibility();
 					break;
 				case 5:
 					Main.mainMenu();
@@ -370,11 +390,11 @@ class Student extends Info {
 
 	void eligibility() {
 
-		System.out.println("Are you getting your scholarship as a student?");
+		System.out.println("\nAre you getting your scholarship as a student?");
 		yesOrNot = yesOrNot(scanner);
 
 		if (yesOrNot.equals("yes")) {
-			System.out.println("\nPlease, enter the amount of your scholarship.");
+			System.out.println("\nPlease, enter the monthly amount of your scholarship.");
 			scholarshipIncome = getPositiveInt(scanner);
 		}
 
@@ -382,7 +402,7 @@ class Student extends Info {
 		yesOrNot = yesOrNot(scanner);
 
 		if (yesOrNot.equals("yes")) {
-			System.out.println("\nPlease, enter the amount of your extra income.");
+			System.out.println("\nPlease, enter the monthly amount of your extra income.");
 			extraIncome = getPositiveInt(scanner);
 		}
 
@@ -403,7 +423,7 @@ class Employed extends Info {
 
 	void eligibility() {
 
-		System.out.print("Please, enter your monthly salary.");
+		System.out.println("What is your monthly salary?");
 		salaryIncome = getPositiveInt(scanner);
 
 		System.out.println("Are you having an extra monthly income (besides the salary)?");
@@ -431,7 +451,7 @@ class Unemployed extends Info {
 
 	void eligibility() {
 
-		System.out.println("Are you getting the monthly unemployement benefits?");
+		System.out.println("\nAre you getting the monthly unemployement benefits?");
 		yesOrNot = yesOrNot(scanner);
 
 		if (yesOrNot.equals("yes")) {
@@ -469,12 +489,12 @@ class Retired extends Info {
 	int pensionIncome;
 	int extraIncome;
 
-	void eligiblility() {
+	void eligibility() {
 
-		System.out.print("Please, enter the amount of your monthly pension.");
+		System.out.println("\nPlease, enter the amount of your monthly pension.");
 		pensionIncome = getPositiveInt(scanner);
 
-		System.out.println("Are you having an extra monthly income (besides the pension)?");
+		System.out.println("\nAre you having an extra monthly income (besides the pension)?");
 		yesOrNot = yesOrNot(scanner);
 
 		if (yesOrNot.equals("yes")) {
@@ -493,46 +513,126 @@ class Retired extends Info {
 }
 
 class Registered extends Info {
-	
-	static void account() {
+
+	static void accessAccount() {
 
 		int attempts = 3;
-		
-		System.out.println("In order to enter to your personal account we need your personal/ registered contact phone number.");
-		phoneNumber = getValidPhoneNumber(scanner);
-				
-		if (!list.containsKey(phoneNumber)) {
-			System.out.println("It looks like you are not registered yet. You are back to the main Menu.");
+
+		System.out.println(
+				"\nIn order to enter to your personal account we need your personal/ registered contact phone number.");
+		phoneNumber = getPositiveInt(scanner);
+
+		if (!profiles.containsKey(phoneNumber)) {
+			System.out.println("It looks like you are not registered yet. You are back to the main Menu.\n");
 			Main.mainMenu();
 		}
-		
-		if (list.containsKey(phoneNumber)) {
-			
+
+		if (profiles.containsKey(phoneNumber)) {
+
 			System.out.println("\nThank you.\nPlease, enter the password. You have the 3 attempts.");
-			
-			while (attempts > 0) {
-				System.out.print("Enter your password: ");
 
-	            // Check if the input is a valid password
-	            if (scanner.hasNextLine()) {
-	                String userGuess = scanner.nextLine();
+			for (int attempt = 1; attempt <= 3; attempt++) {
+			    System.out.print("Enter your password: ");
 
-	                // Check if the input is correct
-	                if (userGuess == list.get(phoneNumber).getPassword())  {
-	                    System.out.println("Thank you for the successful input!");
-	                    return;
-	                } else {
-	                    System.out.println("Incorrect input. Try again.");
-	                    attempts--;
-	                    System.out.println("Attempts left: " + attempts);
-	                }
-	            } else {
-	                System.out.println("Invalid input.");
-	                scanner.next(); // Consume the invalid input to avoid an infinite loop
-	            }
-	        }
+			    String userInput = scanner.nextLine().trim();
+			    userInput = scanner.nextLine();
 
-	        System.out.println("Sorry, you've run out of attempts.");
-	    }
+			    if (userInput.equals(profiles.get(phoneNumber).getPassword())) {
+			        System.out.println("Thank you for the successful input!");
+			        manageAccount();
+			        return; // exit the method after successful input
+			    } else {
+			        System.out.println("Incorrect input. Attempts left: " + (3 - attempt));
+			    }
+			}
+			System.out.println("Sorry, you've run out of attempts.");
+			Main.mainMenu();
+		}
+	}
+
+	// The Menu of the profile
+	static void manageAccount() {
+
+		System.out.println(name + ", welcome to your account!\n");
+		int choice;
+
+		do {
+			System.out.println(
+					"\n\tMenu:\n1. See the current status of the application;\n2. Amend the name;\n3. Amend the age;"
+							+ "\n4. Amend the contact phone number;\n5. Amend the password;\n6. Delete the profile;\n7. Back to the main Menu;"
+							+ "\n0. Exit the program;");
+
+			try {
+				System.out.print("Enter your choice: ");
+
+				choice = scanner.nextInt();
+
+				switch (choice) {
+				case 1:
+					System.out.println(profiles.get(phoneNumber).getStatus());
+					break;
+				case 2:
+					System.out.println("Enter your new name.");
+					String newName = getAllLetters(scanner);
+					profiles.get(phoneNumber).setName(newName);
+					System.out.println(newName + ", your profile has been successfully updated.");
+					System.out.println(profiles.get(phoneNumber));
+					break;
+				case 3:
+					System.out.println("Enter your age.");
+					int newAge = getPositiveInt(scanner);
+					profiles.get(phoneNumber).setAge(newAge);
+					System.out.println(name + ", your profile has been successfully updated.");
+					System.out.println(profiles.get(phoneNumber));
+					break;
+				case 4:
+					System.out.println("Enter your personal phone number.");
+					int newPhoneNumber = getValidPhoneNumber(scanner);
+
+					if (profiles.containsKey(newPhoneNumber)) {
+						System.out.println("The phone number you entered is not valid for the amendment.");
+						manageAccount();
+					}
+
+					profiles.put(newPhoneNumber,
+							new Info(profiles.get(phoneNumber).getName(), profiles.get(phoneNumber).getAge(),
+									profiles.get(phoneNumber).getPassword(), profiles.get(phoneNumber).getStatus()));
+
+					profiles.remove(phoneNumber); // remove the profile with the old key, i.e., the old phone number
+					System.out.println(name + ", your profile has been successfully updated.");
+					System.out.println(profiles.get(newPhoneNumber)); // print out the updated info of the profile																			// profile
+					break;
+				case 5:
+					System.out.println("Enter your new password.");
+					String newPassword = getValidPassword(scanner);
+					profiles.get(phoneNumber).setPassword(newPassword);
+					System.out.println(name + ", your profile has been successfully updated.");
+					System.out.println(profiles.get(phoneNumber));
+					Main.mainMenu();
+					break;
+				case 6:
+					System.out.println(name + ", your profile has been removed.");
+					profiles.remove(phoneNumber);
+					Main.mainMenu();
+					break;
+				case 7:
+					Main.mainMenu();
+					break;
+				case 0:
+					System.out.println("Thank you for your session!");
+					break;
+				default:
+					System.out.println("Invalid choice. Please enter a valid option.");
+					break;
+				}
+			} catch (InputMismatchException e) {
+				// Handle the exception when the user enters a non-integer value
+				System.out.println("Invalid input. Please enter a valid option.");
+				// Clear the scanner buffer
+				scanner.next();
+				// Set choice to a value that will loop again
+				choice = -1;
+			}
+		} while (choice != 0);
 	}
 }
