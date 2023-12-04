ea3d48c765db41f4b4b844e98d987f8518b9ad3e
Albert Cordina <albert.cordina@dci-student.org>
2023-12-04 16:41:02 +0100

    asdf

--- Training/src/singleton/Singleton_rules.java
+++ Training/src/singleton/Singleton_rules.java
@@ -1,13 +1,13 @@
 package singleton;
 /*
  * Singleton is a design pattern that ensures a class has only one instance 
- *           and provides a global point of access to that instance. 
+ * and provides a global point of access to that instance. 
  * This pattern is useful when exactly one object is needed to coordinate actions across the system.
 
 Here's a simple example of a Singleton in Java:
  * 
  */
-public class Singleton {
+class Singleton {
 /*
  * In this example:
  * 
@@ -19,15 +19,22 @@ public class Singleton {
    4. The showMessage() method is an example of a method that can be called on the singleton instance.
  * 
  */
-    // The single instance of the class
+    // Create the single instance of the class, NOTE: must be 'private'
     private static Singleton instance;
 
-    // Private constructor to prevent instantiation from outside the class
+    // Create the 'private' constructor to prevent instantiation from outside the class
     private Singleton() {
         // Initialization code, if needed
-    }
+    }  //  Note: must be 'private'
 
-    // Public method to provide the global point of access to the instance
+    /*
+     * Public method to provide the global point of access to the instance,
+     * i.e. the only way to get an instance of this class is via this method.
+     *  Must be 'public' and 'static'.
+     *  Therefore, this method is the only 'key' to get an access for other classes!
+     *   see below the class 'Singleton_rules'.
+     */
+    //  
     public static Singleton getInstance() {
         if (instance == null) {
             instance = new Singleton();
@@ -48,3 +55,28 @@ public class Singleton {
      * 
      */
 }
+class Singleton_rules {
+	/*
+	 * In the direct way singleton1 and singleton2 are references to two separate instances of 
+	 * the Singleton class. Such code doesn't implement the Singleton pattern, as it allows 
+	 * the creation of multiple instances directly using the new keyword:
+	 * 
+	 * Syngletone singletone1 = new Singleton ();
+	 * Syngletone singletone2 = new Singleton ();
+	 * 
+	 * With Singletone the both singleton1 and singleton2 are references 
+	 *   to the same instance of the Singleton class:
+	 */
+	public static void main(String[] args) {
+		
+		Singleton singletone1 = Singleton.getInstance();
+		singletone1.showMessage();
+		
+		Singleton singletone2 = Singleton.getInstance();
+		singletone2.showMessage();
+		
+		
+	}
+}
+
+
